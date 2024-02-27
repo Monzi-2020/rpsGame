ae4ed754bf957ec1cba819689b16960de4e15f20
Monzi2020 <nyenuen@gmail.com>
2024-02-27 17:35:55 +1100

    Put the return values into score variables Fixed the syntax to return the values into the score variables. Edited <score board> for better view. Declared endGame() which hasn't completed.

--- javascript.js
+++ javascript.js
@@ -6,8 +6,9 @@ function getComputerChoice() {
     return choice;
     }
 
-let scoreComputer; 
-let scorePlayer;
+const scoreComputer = [];
+const scorePlayer = [];
+
 
 function playGame() {
     for (let i = 0; i < 5; i++) {
@@ -15,60 +16,58 @@ function playGame() {
         const computerSelection = getComputerChoice();
         const playerSelection = prompt("Rock? Paper? Scissors?");
         playRound(computerSelection, playerSelection);
-        console.log(`Computer: ${scoreComputer} You: ${scorePlayer}`); 
+        console.log(`      <Score Board>
+    Computer: ${scoreComputer} You: ${scorePlayer}`); 
     } 
    }
 
-// function endGame() {
-    // console.log()
-// }
+function endGame() {
+    let winner;
+    console.log(`The winner is ${winner}! Congratulations!`);
+    }
 
+    
 function playRound(computerSelection, playerSelection) {
         if (computerSelection === "rock" && playerSelection.toLowerCase() === "scissors") {
             console.log("You lose! Rock beats scissors");
-            scoreComputer = +1;
-            scorePlayer = +0;
-            return [ scoreComputer, scorePlayer];
+            scoreComputer.push("1");
+            scorePlayer.push("0");
+            
         }
         else if (computerSelection === "paper" && playerSelection.toLowerCase() === "rock") {
-            console.log("You lose! Paper beats rock"); 
-            scoreComputer = +1;
-            scorePlayer = +0;
-            return [ scoreComputer, scorePlayer]; 
+            console.log("You lose! Paper beats rock");          
+            scoreComputer.push("1");
+            scorePlayer.push("0");
             
         }
         else if (computerSelection === "scissors" && playerSelection.toLowerCase() === "paper") {
-            console.log("You lose! scissors beats paper"); 
-            scoreComputer = +1;
-            scorePlayer = +0;
-            return [ scoreComputer, scorePlayer];  
+            console.log("You lose! scissors beats paper");          
+            scoreComputer.push("1");
+            scorePlayer.push("0");
+            
         }    
         else if (playerSelection === "rock" && computerSelection.toLowerCase() === "scissors") {
-            console.log("You win! Rock beats scissors");
-            scoreComputer = +0;
-            scorePlayer = +1;
-            return [ scoreComputer, scorePlayer]; 
+            console.log("You win! Rock beats scissors");       
+            scoreComputer.push("0");
+            scorePlayer.push("1");
             
         }
         else if (playerSelection === "paper" && computerSelection.toLowerCase() === "rock") {
-            console.log("You win! Paper beats rock");
-            scoreComputer = +0;
-            scorePlayer = +1;
-            return [ scoreComputer, scorePlayer];
+            console.log("You win! Paper beats rock");         
+            scoreComputer.push("0");
+            scorePlayer.push("1");
              
         }
         else if (playerSelection === "scissors" && computerSelection.toLowerCase() === "paper") {
-            console.log("You win! scissors beats paper");
-            scoreComputer = +0;
-            scorePlayer = +1;
-            return [ scoreComputer, scorePlayer];
+            console.log("You win! scissors beats paper");    
+            scoreComputer.push("0");
+            scorePlayer.push("1");
             
         }
         else {
             console.log("Tied. Try again!")
-            scoreComputer = +0;
-            scorePlayer = +0;
-            return [ scoreComputer, scorePlayer];
+            scoreComputer.push("0");
+            scorePlayer.push("0");
         }
     }
     
