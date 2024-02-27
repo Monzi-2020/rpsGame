c07cfe3792a4de65e2ac670666869fdd2fcc645c
Monzi2020 <nyenuen@gmail.com>
2024-02-27 15:22:06 +1100

    Added playGame() Repeat 5 times. Every function works. Need to make the return value to be assigned to score variables.

--- javascript.js
+++ javascript.js
@@ -7,32 +7,42 @@ function getComputerChoice() {
     }
 
 const computerSelection = getComputerChoice();
-const playerSelection = prompt("Rock? Paper? Scissors?").toLowerCase();
-let scoreComputer;
-let scorePlayer;
-  
-    function playRound(computerSelection, playerSelection) {
-        if (computerSelection === "rock" && playerSelection === "scissors") {
+
+let scoreComputer = [];
+let scorePlayer = [];
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock? Paper? Scissors?");
+        getComputerChoice();
+        playRound(computerSelection, playerSelection);
+        console.log(scoreComputer,scorePlayer); 
+    } 
+   }
+   
+
+function playRound(computerSelection, playerSelection) {
+        if (computerSelection === "rock" && playerSelection.toLowerCase() === "scissors") {
             console.log("You lose! Rock beats scissors");
             return { scoreComputer: 1, scorePlayer: 0 };
         }
-        else if (computerSelection === "paper" && playerSelection === "rock") {
+        else if (computerSelection === "paper" && playerSelection.toLowerCase() === "rock") {
             console.log("You lose! Paper beats rock");  
             return { scoreComputer: 1, scorePlayer: 0 };
         }
-        else if (computerSelection === "scissors" && playerSelection=== "paper") {
+        else if (computerSelection === "scissors" && playerSelection.toLowerCase() === "paper") {
             console.log("You lose! scissors beats paper");   
             return { scoreComputer: 1, scorePlayer: 0 }; 
         }    
-        else if (playerSelection === "rock" && computerSelection === "scissors") {
+        else if (playerSelection === "rock" && computerSelection.toLowerCase() === "scissors") {
             console.log("You win! Rock beats scissors");
             return { scoreComputer: 0, scorePlayer: 1 };
         }
-        else if (playerSelection === "paper" && computerSelection === "rock") {
+        else if (playerSelection === "paper" && computerSelection.toLowerCase() === "rock") {
             console.log("You win! Paper beats rock");
             return { scoreComputer: 0, scorePlayer: 1 };  
         }
-        else if (playerSelection === "scissors" && computerSelection=== "paper") {
+        else if (playerSelection === "scissors" && computerSelection.toLowerCase() === "paper") {
             console.log("You win! scissors beats paper");
             return { scoreComputer: 0, scorePlayer: 1 };
         }
@@ -41,5 +51,8 @@ let scorePlayer;
             return { scoreComputer: 0, scorePlayer: 0 };
         }
     }
-   console.log(scoreComputer,scorePlayer);
+    
+
+  
+
 
