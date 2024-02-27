f68d19faf64ba697c020376741d546778efa3662
Monzi2020 <nyenuen@gmail.com>
2024-02-27 12:12:34 +1100

    Completed playRound() working

--- javascript.js
+++ javascript.js
@@ -10,5 +10,25 @@ const computerSelection = getComputerChoice();
 const playerSelection = prompt("Rock? Paper? Scissors?").toLowerCase();
 
 function playRound(computerSelection, playerSelection) {
-
+    if (computerSelection === "rock" && playerSelection === "scissors") {
+        console.log("You lose! Rock beats scissors");
+    }
+    else if (computerSelection === "paper" && playerSelection === "rock") {
+        console.log("You lose! Paper beats rock");   
+    }
+    else if (computerSelection === "scissors" && playerSelection=== "paper") {
+        console.log("You lose! scissors beats papaer");    
+    }    
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("You win! Rock beats scissors");
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("You win! Paper beats rock");  
+    }
+    else if (playerSelection === "scissors" && computerSelection=== "paper") {
+        console.log("You win! scissors beats paper");
+    }
+    else {
+        console.log("Tied. Try again!")
+    }
 }
