f3aeff8485e987560fc0f761b37e3fc6fb652750
Monzi2020 <nyenuen@gmail.com>
2024-02-27 16:12:27 +1100

    Fixed getComputerChoice in playGame() getComputerChoice chose same option every round. Put the computerSelection variable in the playGame() and change the order with getComputerChoice.

--- javascript.js
+++ javascript.js
@@ -6,14 +6,14 @@ function getComputerChoice() {
     return choice;
     }
 
-const computerSelection = getComputerChoice();
-let scoreComputer = [];
-let scorePlayer = [];
+let scoreComputer; 
+let scorePlayer;
 
 function playGame() {
     for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Rock? Paper? Scissors?");
         getComputerChoice();
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("Rock? Paper? Scissors?");
         playRound(computerSelection, playerSelection);
         console.log(`Computer: ${scoreComputer} You: ${scorePlayer}`); 
     } 
@@ -26,48 +26,48 @@ function playGame() {
 function playRound(computerSelection, playerSelection) {
         if (computerSelection === "rock" && playerSelection.toLowerCase() === "scissors") {
             console.log("You lose! Rock beats scissors");
-            scoreComputer = 1;
-            scorePlayer = 0;
+            scoreComputer = +1;
+            scorePlayer = +0;
             return [ scoreComputer, scorePlayer];
         }
         else if (computerSelection === "paper" && playerSelection.toLowerCase() === "rock") {
             console.log("You lose! Paper beats rock"); 
-            scoreComputer = 1;
-            scorePlayer = 0;
+            scoreComputer = +1;
+            scorePlayer = +0;
             return [ scoreComputer, scorePlayer]; 
             
         }
         else if (computerSelection === "scissors" && playerSelection.toLowerCase() === "paper") {
             console.log("You lose! scissors beats paper"); 
-            scoreComputer = 1;
-            scorePlayer = 0;
+            scoreComputer = +1;
+            scorePlayer = +0;
             return [ scoreComputer, scorePlayer];  
         }    
         else if (playerSelection === "rock" && computerSelection.toLowerCase() === "scissors") {
             console.log("You win! Rock beats scissors");
-            scoreComputer = 0;
-            scorePlayer = 1;
+            scoreComputer = +0;
+            scorePlayer = +1;
             return [ scoreComputer, scorePlayer]; 
             
         }
         else if (playerSelection === "paper" && computerSelection.toLowerCase() === "rock") {
             console.log("You win! Paper beats rock");
-            scoreComputer = 0;
-            scorePlayer = 1;
+            scoreComputer = +0;
+            scorePlayer = +1;
             return [ scoreComputer, scorePlayer];
              
         }
         else if (playerSelection === "scissors" && computerSelection.toLowerCase() === "paper") {
             console.log("You win! scissors beats paper");
-            scoreComputer = 0;
-            scorePlayer = 1;
+            scoreComputer = +0;
+            scorePlayer = +1;
             return [ scoreComputer, scorePlayer];
             
         }
         else {
             console.log("Tied. Try again!")
-            scoreComputer = 0;
-            scorePlayer = 0;
+            scoreComputer = +0;
+            scorePlayer = +0;
             return [ scoreComputer, scorePlayer];
         }
     }
