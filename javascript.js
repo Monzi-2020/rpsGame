d6b78f92858e0352f6a6553e2d8583afe3663aff
Monzi2020 <nyenuen@gmail.com>
2024-03-07 15:58:40 +1100

    Fixed the bug of buttonListener: now playerSelection(button.id) changeds when it's clicked. Declared score variables and appened them to content node. it's displayed that result of each round, computer and players current score. But need to make the game stop when one of the scores reach 5.

--- javascript.js
+++ javascript.js
@@ -6,21 +6,19 @@ function getComputerChoice() {
     return choice;
     }
 
-let scoreComputer = 0;
-let scorePlayer = 0;
 
 getComputerChoice();
 const computerSelection = getComputerChoice();
 let playerSelection = ""
-    //     console.log(`      <Score Board>
-    // Computer: ${scoreComputer} You: ${scorePlayer}`); 
+let scoreComputer = 0;
+let scorePlayer = 0;
+
 
 
 // endGame(scoreComputer, scorePlayer);
 
 function endGame(scoreComputer,scorePlayer) {   
     let winner;
-
     if (scoreComputer > scorePlayer) {
         winner = "Computer";
     }
@@ -39,58 +37,75 @@ function playRound(computerSelection, playerSelection) {
             const text ="You lose! Rock beats scissors";
             scoreComputer += 1;
             div.textContent = text;
+            scoreCom.textContent = `Computer score: ${scoreComputer}`;
+            scorePla.textContent = `Your score : ${scorePlayer}`;
     
         }
         else if (computerSelection === "paper" && playerSelection.toLowerCase() === "rock") {
             const text ="You lose! Paper beats rock";          
             scoreComputer += 1; 
             div.textContent = text;
+            scoreCom.textContent = `Computer score: ${scoreComputer}`;
+            scorePla.textContent = `Your score : ${scorePlayer}`;
  
         }
         else if (computerSelection === "scissors" && playerSelection.toLowerCase() === "paper") {
             const text ="You lose! scissors beats paper";          
             scoreComputer += 1;
             div.textContent = text;
+            scoreCom.textContent = `Computer score: ${scoreComputer}`;
+            scorePla.textContent = `Your score : ${scorePlayer}`;
             
         }    
         else if (playerSelection === "rock" && computerSelection.toLowerCase() === "scissors") {
             const text ="You win! Rock beats scissors";       
             scorePlayer += 1;
             div.textContent = text;
+            scoreCom.textContent = `Computer score: ${scoreComputer}`;
+            scorePla.textContent = `Your score : ${scorePlayer}`;
             
         }
         else if (playerSelection === "paper" && computerSelection.toLowerCase() === "rock") {
             const text ="You win! Paper beats rock";         
             scorePlayer += 1;
             div.textContent = text;
+            scoreCom.textContent = `Computer score: ${scoreComputer}`;
+            scorePla.textContent = `Your score : ${scorePlayer}`;
              
         }
         else if (playerSelection === "scissors" && computerSelection.toLowerCase() === "paper") {
             const text ="You win! scissors beats paper";    
             scorePlayer += 1;
             div.textContent = text;
+            scoreCom.textContent = `Computer score: ${scoreComputer}`;
+            scorePla.textContent = `Your score : ${scorePlayer}`;
             
         }
         else {
             const text ="Tied. Try again!"
             div.textContent = text;
+            scoreCom.textContent = `Computer score: ${scoreComputer}`;
+            scorePla.textContent = `Your score : ${scorePlayer}`;
         
         }
-     content.appendChild(div)
-    }
+     content.appendChild(div);
+     content.appendChild(scoreCom);
+     content.appendChild(scorePla);
+}
     
 
 const content = document.querySelector("content")
 const buttons = document.querySelectorAll("button");
 const div = document.createElement("div");
+const scoreCom = document.createElement("div");
+const scorePla = document.createElement("div");
 
 buttons.forEach((button) => {
-    playerSelection = button.id;
     button.addEventListener('click', () => {
+        playerSelection = button.id;
         playRound(computerSelection, playerSelection);
     });
 });
-
 
 
 
